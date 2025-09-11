62e91c874383fb95b9e30ec346bf84c70ee6a35b
SmileJakoby <46690956+SmileJakoby@users.noreply.github.com>
2025-09-11 17:42:43 -0600

    Generalized 'generateMoves' function passes all tests for Bishop!

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -46,6 +46,30 @@ public class ChessPiece {
         return type;
     }
 
+
+    private void generateMoves(ChessBoard board, ChessPosition startingPos, ChessPosition currentPos, HashSet<ChessMove> givenSet, int rowAdd, int colAdd, boolean recursive){
+        int row = currentPos.getRow() + rowAdd;
+        int col = currentPos.getColumn() + colAdd;
+        if (row <= 8 && row >= 1 && col <= 8 && col >= 1) {
+            ChessPosition nextPos = new ChessPosition(row, col);
+            ChessPiece pieceInWay = board.getPiece(nextPos);
+            if (pieceInWay == null) {
+                givenSet.add(new ChessMove(startingPos, nextPos, null));
+                if (recursive) {
+                    generateMoves(board, startingPos, nextPos, givenSet, rowAdd, colAdd, recursive);
+                }
+            }
+            else {
+                if (pieceInWay.getTeamColor() == this.pieceColor) {
+                    return;
+                }
+                if (pieceInWay.getTeamColor() != this.pieceColor) {
+                    givenSet.add(new ChessMove(startingPos, nextPos, null));
+                }
+            }
+        }
+        return;
+    }
     /**
      * Calculates all the positions a chess piece can move to
      * Does not take into account moves that are illegal due to leaving the king in
@@ -53,55 +77,19 @@ public class ChessPiece {
      *
      * @return Collection of valid moves
      */
-    private Collection<ChessMove> generateMoves(ChessBoard board, ChessPosition startingPos, ChessPosition currentPos, HashSet<ChessMove> givenSet, int rowAdd, int colAdd, boolean recursive){
-        int row = currentPos.getRow() + rowAdd;
-        int col = currentPos.getColumn() + colAdd;
-        ChessPosition nextPos = new ChessPosition(row,col);
-        if (board.getPiece(nextPos) == null){
-            givenSet.add(new ChessMove(startingPos, nextPos, null));
-            if (recursive) {
-                generateMoves(board, startingPos, nextPos, givenSet, rowAdd, colAdd, recursive);
-            }
-        }
-        return givenSet;
-    }
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
         ChessPiece piece = board.getPiece(myPosition);
         HashSet<ChessMove> returnList = new HashSet<>();
         switch(piece.getPieceType()) {
             case BISHOP:
-                int row = myPosition.getRow();
-                int col = myPosition.getColumn();
-                //Diagonal Left and Down
-                while (row > 1 && col > 1){
-                    row -= 1;
-                    col -= 1;
-                    returnList.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(row,col), null));
-                }
-                row = myPosition.getRow();
-                col = myPosition.getColumn();
-                //Diagonal Left and Up
-                while (row < 8 && col > 1){
-                    row += 1;
-                    col -= 1;
-                    returnList.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(row,col), null));
-                }
-                row = myPosition.getRow();
-                col = myPosition.getColumn();
-                //Diagonal Right and Up
-                while (row < 8 && col < 8){
-                    row += 1;
-                    col += 1;
-                    returnList.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(row,col), null));
-                }
-                row = myPosition.getRow();
-                col = myPosition.getColumn();
-                //Diagonal Right and Down
-                while (row > 1 && col < 8){
-                    row -= 1;
-                    col += 1;
-                    returnList.add(new ChessMove(new ChessPosition(myPosition.getRow(), myPosition.getColumn()), new ChessPosition(row,col), null));
-                }
+                //Down and left
+                generateMoves(board, myPosition, myPosition, returnList, -1, -1, true);
+                //Down and right
+                generateMoves(board, myPosition, myPosition, returnList, -1, 1, true);
+                //Up and right
+                generateMoves(board, myPosition, myPosition, returnList, 1, 1, true);
+                //Up and left
+                generateMoves(board, myPosition, myPosition, returnList, 1, -1, true);
                 return returnList;
             case KNIGHT:
                 return List.of();
