19fc5d78e4df07c36880d7a9852617e9a0352cb0
SmileJakoby <46690956+SmileJakoby@users.noreply.github.com>
2025-11-09 20:32:19 -0700

    Help implemented

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,9 +1,51 @@
 import chess.*;
 
+import java.util.Scanner;
+
+import static ui.EscapeSequences.*;
+
 public class Main {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
-        System.out.println("This is a test");
+        Scanner inputScanner = new Scanner(System.in);
+        System.out.println("♕ 240 Chess Client. Type 'help' to get started.");
+        LoggedOutLoop(inputScanner);
+        //Logged out
+
+    }
+
+    public static void LoggedOutLoop(Scanner inputScanner)
+    {
+        boolean stayInLoop = true;
+        while (stayInLoop){
+            System.out.print("[LOGGED OUT] >>> ");
+            String command = inputScanner.nextLine();
+            command = command.toLowerCase();
+            switch (command) {
+                case "help":
+                    System.out.println("  " + SET_TEXT_COLOR_BLUE + "register <USERNAME> <PASSWORD> <EMAIL>"
+                            + SET_TEXT_COLOR_MAGENTA + " - Register a new account.");
+                    System.out.println("  " + SET_TEXT_COLOR_BLUE + "login <USERNAME> <PASSWORD>"
+                            + SET_TEXT_COLOR_MAGENTA + " - Log into an existing account.");
+                    System.out.println("  " + SET_TEXT_COLOR_BLUE + "help"
+                            + SET_TEXT_COLOR_MAGENTA + " - see a list of available commands.");
+                    System.out.println("  " + SET_TEXT_COLOR_BLUE + "quit"
+                            + SET_TEXT_COLOR_MAGENTA + " - exit the program." + SET_TEXT_COLOR_WHITE);
+
+                    break;
+                case "login":
+                    System.out.println("Login not implemented yet");
+                    break;
+                case "register":
+                    System.out.println("Register not implemented yet");
+                    break;
+                case "quit":
+                    System.out.println("Quit not implemented yet");
+                    stayInLoop = false;
+                    break;
+                default:
+                    System.out.println("Unrecognized command");
+                    break;
+            }
+        }
     }
 }
