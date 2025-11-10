64b2c738ecedff49ebe488f4a396c027ae73eca8
SmileJakoby <46690956+SmileJakoby@users.noreply.github.com>
2025-11-10 09:03:55 -0700

    ahhh I forgot to commit for an hour. Logged_out portion of CLI fully implemented. Logged_in portion has help, logout, and createGame.

--- client/src/main/java/client/ChessClient.java
+++ client/src/main/java/client/ChessClient.java
@@ -1,7 +1,9 @@
 package client;
 
 import com.google.gson.Gson;
+import datamodel.CreateGameResponse;
 import datamodel.RegisterResponse;
+import model.GameData;
 import model.UserData;
 
 import java.util.Scanner;
@@ -16,8 +18,8 @@ import java.net.http.HttpResponse;
 
 public class ChessClient {
 
-    private static final int SIGNED_OUT = 0;
-    private static final int SIGNED_IN = 1;
+    private static final int LOGGED_OUT = 0;
+    private static final int LOGGED_IN = 1;
 
     private static String serverUrl = "";
 
@@ -25,7 +27,7 @@ public class ChessClient {
 
     private static String myAuthToken = "";
 
-    private static int authState = SIGNED_OUT;
+    private static int authState = LOGGED_OUT;
 
     public ChessClient(String givenServerUrl){
         serverUrl = givenServerUrl;
@@ -35,12 +37,12 @@ public class ChessClient {
         System.out.println("♕ 240 Chess Client. Type 'help' to get started.");
 
         String result = "";
-        while (!result.equals("quit")) {
-            if (authState == SIGNED_OUT) {
-                System.out.print("[SIGNED OUT] >>> ");
+        while (!result.equals("Goodbye!")) {
+            if (authState == LOGGED_OUT) {
+                System.out.print("[LOGGED OUT] >>> ");
             }
-            if (authState == SIGNED_IN) {
-                System.out.print("[SIGNED IN] >>> ");
+            if (authState == LOGGED_IN) {
+                System.out.print("[LOGGED IN] >>> ");
             }
             String line = inputScanner.nextLine();
             line = line.toLowerCase();
@@ -53,54 +55,43 @@ public class ChessClient {
                 System.out.print(msg);
             }
         }
-
-
     }
 
     private static String EvalCLI(String givenString)
     {
         var commands = givenString.split(" ");
-        switch (commands[0]) {
-            case "help":
-                return printHelp();
-            case "login":
-                System.out.println("Login not implemented yet");
-                break;
-            case "register":
-                if (commands.length >= 4)
-                    try{
-                        UserData newUser = new UserData(commands[1], commands[3], commands[2]);
-                        var jsonBody = new Gson().toJson(newUser);
-                        HttpRequest request = HttpRequest.newBuilder()
-                                .uri(new URI(serverUrl + "/user"))
-                                .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
-                                .header("Content-Type", "application/json")
-                                .build();
-                        HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-                        if (response.statusCode() == 200) {
-                            RegisterResponse registerResponse = new Gson().fromJson(response.body(), RegisterResponse.class);
-                            myAuthToken = registerResponse.authToken();
-                            authState = SIGNED_IN;
-                            return registerResponse.toString();
-                        }
-                        else{
-                            System.out.println("Error: received status code " + response.statusCode());
-                        }
-                    }
-                    catch (Exception e){
-                        System.out.println(e.getMessage());
-                    }
-                else{
-                    return "Must provide a Username, Password, and Email.";
-                }
-                break;
-            case "quit":
-                return "quit";
+        if (authState == LOGGED_OUT) {
+            switch (commands[0]) {
+                case "help":
+                    return PrintHelp();
+                case "register":
+                    return Register(commands);
+                case "login":
+                    return Login(commands);
+                case "quit":
+                    return "Goodbye!";
+            }
+        }
+        if (authState == LOGGED_IN) {
+            switch (commands[0]) {
+                case "help":
+                    return PrintHelp();
+                case "logout":
+                    return Logout();
+                case "create":
+                    return CreateGame(commands);
+                case "list":
+                    return "list games not implemented yet";
+                case "join":
+                    return "join game not implemented yet";
+                case "observe":
+                    return "observe game not implemented yet";
+            }
         }
         return "Unrecognized command";
     }
-    private static String printHelp(){
-        if (authState == SIGNED_OUT){
+    private static String PrintHelp(){
+        if (authState == LOGGED_OUT){
             return ("  " + SET_TEXT_COLOR_BLUE + "register <USERNAME> <PASSWORD> <EMAIL>"
                     + SET_TEXT_COLOR_MAGENTA + " - Register a new account.\n" +
                     "  " + SET_TEXT_COLOR_BLUE + "login <USERNAME> <PASSWORD>" +
@@ -108,11 +99,142 @@ public class ChessClient {
                     "  " + SET_TEXT_COLOR_BLUE + "help"
                     + SET_TEXT_COLOR_MAGENTA + " - see a list of available commands.\n"
                     + "  " + SET_TEXT_COLOR_BLUE + "quit"
-                    + SET_TEXT_COLOR_MAGENTA + " - exit the program.\n" + SET_TEXT_COLOR_WHITE);
+                    + SET_TEXT_COLOR_MAGENTA + " - exit the program." + SET_TEXT_COLOR_WHITE);
         }
         else {
-            return ("  " + SET_TEXT_COLOR_BLUE + "create <NAME>"
-                    + SET_TEXT_COLOR_MAGENTA + " - Create a game.\n");
+            return ("  " + SET_TEXT_COLOR_BLUE + "help"
+                    + SET_TEXT_COLOR_MAGENTA + " - see a list of available commands.\n" +
+                    "  " + SET_TEXT_COLOR_BLUE + "logout"
+                    + SET_TEXT_COLOR_MAGENTA + " - Logout of your account.\n" +
+                    "  " + SET_TEXT_COLOR_BLUE + "create <NAME>"
+                    + SET_TEXT_COLOR_MAGENTA + " - Create a game.\n" +
+                    "  " + SET_TEXT_COLOR_BLUE + "list"
+                    + SET_TEXT_COLOR_MAGENTA + " - Show available games.\n" +
+                    "  " + SET_TEXT_COLOR_BLUE + "join <ID> [WHITE|BLACK]"
+                    + SET_TEXT_COLOR_MAGENTA + " - Join a game as your choice of color.\n" +
+                    "  " + SET_TEXT_COLOR_BLUE + "observe <ID>"
+                    + SET_TEXT_COLOR_MAGENTA + " - Join a game as an observer." + SET_TEXT_COLOR_WHITE);
+        }
+    }
+    private static String Register(String[] commands){
+        if (commands.length >= 4)
+            try{
+                UserData newUser = new UserData(commands[1], commands[3], commands[2]);
+                var jsonBody = new Gson().toJson(newUser);
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(new URI(serverUrl + "/user"))
+                        .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
+                        .header("Content-Type", "application/json")
+                        .build();
+                HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+                if (response.statusCode() == 200) {
+                    RegisterResponse registerResponse = new Gson().fromJson(response.body(), RegisterResponse.class);
+                    myAuthToken = registerResponse.authToken();
+                    authState = LOGGED_IN;
+                    return "Registered " + registerResponse.username() + "\nWelcome to Chess!";
+                }
+                else{
+                    ErrorMessage errorMessage = new Gson().fromJson(response.body(), ErrorMessage.class);
+                    return errorMessage.message();
+                }
+            }
+            catch (Exception e){
+                return e.getMessage();
+            }
+        else{
+            return "Must provide a Username, Password, and Email.";
+        }
+    }
+
+    private static String Login(String[] commands){
+        if (commands.length >= 3)
+            try{
+                UserData newUser = new UserData(commands[1], null, commands[2]);
+                var jsonBody = new Gson().toJson(newUser);
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(new URI(serverUrl + "/session"))
+                        .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
+                        .header("Content-Type", "application/json")
+                        .build();
+                HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+                if (response.statusCode() == 200) {
+                    RegisterResponse registerResponse = new Gson().fromJson(response.body(), RegisterResponse.class);
+                    myAuthToken = registerResponse.authToken();
+                    authState = LOGGED_IN;
+                    return "Logged in as " + registerResponse.username() + "\nWelcome to Chess!";
+                }
+                else{
+                    ErrorMessage errorMessage = new Gson().fromJson(response.body(), ErrorMessage.class);
+                    return errorMessage.message();
+                }
+            }
+            catch (Exception e){
+                return e.getMessage();
+            }
+        else{
+            return "Must provide a Username and Password.";
+        }
+    }
+
+    private static String Logout(){
+        try{
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI(serverUrl + "/session"))
+                    .DELETE()
+                    .header("Content-Type", "application/json")
+                    .header("authorization", myAuthToken)
+                    .build();
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() == 200) {
+                myAuthToken = "";
+                authState = LOGGED_OUT;
+                return "Logged out";
+            }
+            else{
+                ErrorMessage errorMessage = new Gson().fromJson(response.body(), ErrorMessage.class);
+                myAuthToken = "";
+                authState = LOGGED_OUT;
+                return errorMessage.message();
+            }
+        }
+        catch (Exception e){
+            return e.getMessage();
+        }
+    }
+
+    private static String CreateGame(String[] commands){
+        if (commands.length >= 2)
+            try{
+                StringBuilder fullNameBuilder = new StringBuilder();
+                fullNameBuilder.append(commands[1]);
+                for (int i = 2; i < commands.length; i++){
+                    fullNameBuilder.append(" ");
+                    fullNameBuilder.append(commands[i]);
+                }
+                String fullName = fullNameBuilder.toString();
+                GameData newGame = new GameData(null, null, null, fullName, null);
+                var jsonBody = new Gson().toJson(newGame);
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(new URI(serverUrl + "/game"))
+                        .POST(HttpRequest.BodyPublishers.ofString(jsonBody))
+                        .header("Content-Type", "application/json")
+                        .header("authorization", myAuthToken)
+                        .build();
+                HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+                if (response.statusCode() == 200) {
+                    CreateGameResponse createGameResponse = new Gson().fromJson(response.body(), CreateGameResponse.class);
+                    return "Created game with ID of " + createGameResponse.gameID();
+                }
+                else{
+                    ErrorMessage errorMessage = new Gson().fromJson(response.body(), ErrorMessage.class);
+                    return errorMessage.message();
+                }
+            }
+            catch (Exception e){
+                return e.getMessage();
+            }
+        else{
+            return "Must provide a Name.";
         }
     }
 }
