e4dcd5312c1776b539b01425413d7043b94f7044
SmileJakoby <46690956+SmileJakoby@users.noreply.github.com>
2025-11-26 12:34:49 -0700

    Added makeMove to GameService.java

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,6 +1,8 @@
 package service;
 
 import chess.ChessGame;
+import chess.ChessMove;
+import chess.InvalidMoveException;
 import dataaccess.DataAccess;
 
 import dataaccess.DataAccessException;
@@ -11,7 +13,7 @@ import datamodel.GamesListResponse;
 import model.AuthData;
 import model.GameData;
 
-
+import java.util.Objects;
 
 
 public class GameService {
@@ -86,5 +88,48 @@ public class GameService {
             }
         }
         dataAccess.addPlayer(gameID, dataAccess.getAuthData(givenAuth.authToken()).username(), playerColor);
+    }
+
+    public void makeMove(AuthData givenAuth, ChessMove givenMove, Integer gameID) throws DataAccessException, UnauthorizedException, BadRequestException {
+        String authUsername;
+        if (dataAccess.getAuthData(givenAuth.authToken()) == null)
+        {
+            throw new UnauthorizedException("unauthorized");
+        }
+        else {
+            authUsername = dataAccess.getAuthData(givenAuth.authToken()).username();
+        }
+        if (dataAccess.getGame(gameID) == null || gameID == null)
+        {
+            throw new BadRequestException("Can't find that game");
+        }
+        GameData foundGameData = dataAccess.getGame(gameID);
+        ChessGame.TeamColor colorOfPiece = foundGameData.game().getBoard().getPiece(givenMove.getStartPosition()).getTeamColor();
+        boolean isProperPlayer = false;
+        if (Objects.equals(authUsername, foundGameData.whiteUsername()))
+        {
+            if (colorOfPiece == ChessGame.TeamColor.WHITE)
+            {
+                isProperPlayer = true;
+            }
+        }
+        if (Objects.equals(authUsername, foundGameData.blackUsername()))
+        {
+            if (colorOfPiece == ChessGame.TeamColor.BLACK)
+            {
+                isProperPlayer = true;
+            }
+        }
+        if (!isProperPlayer)
+        {
+            throw new BadRequestException("That is not your piece");
+        }
+        try{
+            foundGameData.game().makeMove(givenMove);
+            dataAccess.updateGame(gameID, foundGameData);
+        }
+        catch(InvalidMoveException e){
+            throw new BadRequestException("Invalid Move");
+        }
     }
 }
